d49686d210875911387291de995e22af4bb04ed8
NJM1290 <juliomalinso@967@gamil.com>
2024-11-18 19:37:26 +0800

    infoswiss

--- src/Components/Content Description/Swiss/index.js
+++ src/Components/Content Description/Swiss/index.js
@@ -18,7 +18,7 @@ const InfoSwiss = () => {
 
   return (
     <div className="info">
-      <h3>Swiss Alps, Switzerland</h3>
+      <h3>{infoswiss.title}</h3>
       <p>Enjoy the breathtaking views and outdoor activities.</p>
       <div className="price">$1200</div>
       <a className="read-more" href="#">
